e646c4c9e687001874752e016d4a1b9f2a9d6c53
koebe1 <koe.be.pay@web.de>
2021-08-04 22:00:46 +0200

    implemented slider functionality on main paige

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,50 @@
 // slider -> info
 const info1 = document.getElementById("info-1");
 const infoCard1 = document.querySelector(".info-card-1");
+const slider1ArrLeft = document.getElementById("slider1-arr-left");
+const slider1ArrRight = document.getElementById("slider1-arr-right");
+const slider1CounterCurr = document.getElementById("slider1-counter-current");
+const slider1CounterEnd = document.getElementById("slider1-counter-end");
+const slider1 = document.getElementById("slider-1");
 
+const sliderList1 = [
+  "../ue_bildmaterial/dummy1.jpg",
+  "../ue_bildmaterial/dummy2.jpg",
+  "../ue_bildmaterial/dummy3.jpg",
+  "../ue_bildmaterial/dummy4.jpg"
+];
+// slider function
+let counter = 0;
+slider1CounterEnd.innerHTML = sliderList1.length;
+let slideDown = pictureList => {
+  if (counter === 0) {
+    counter = pictureList.length - 1;
+    slider1CounterCurr.innerHTML = counter + 1;
+  } else {
+    counter -= 1;
+    slider1CounterCurr.innerHTML = counter + 1;
+  }
+  slider1.style.backgroundImage = `url("${pictureList[counter]}")`;
+};
+let slideUp = pictureList => {
+  if (counter === pictureList.length - 1) {
+    counter = 0;
+    slider1CounterCurr.innerHTML = counter + 1;
+  } else {
+    counter += 1;
+    slider1CounterCurr.innerHTML = counter + 1;
+  }
+  slider1.style.backgroundImage = `url("${pictureList[counter]}")`;
+};
+
+slider1ArrLeft.addEventListener("click", () => slideDown(sliderList1));
+slider1ArrRight.addEventListener("click", () => slideUp(sliderList1));
+// function to show the info box of the slider
 let clicked = false;
 let infoSlide = (button, target) => {
   if (!clicked) {
     target.style.display = "flex";
+    target.style.opasity = "0";
     button.innerHTML = "CLOSE";
 
     clicked = true;
