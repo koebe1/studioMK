29a998b88ad6e4453c0f36169693d01199f4ef2a
koebe1 <koe.be.pay@web.de>
2021-08-12 11:57:50 +0200

    started testing for mobile siwpe support

--- scripts/play.js
+++ scripts/play.js
@@ -123,73 +123,75 @@ const gridItemList31 = [
   "../pictures/play/02/02_illi.jpg"
 ];
 
-let listen = () => {
-  gridItem2Left.addEventListener("click", () => {
-    slideDown(gridItemList2, "counter2", gridItem2);
-  });
-  gridItem2Right.addEventListener("click", () => {
-    slideUp(gridItemList2, "counter2", gridItem2);
-  });
-  gridItem4Left.addEventListener("click", () => {
-    slideDown(gridItemList4, "counter4", gridItem4);
-  });
-  gridItem4Right.addEventListener("click", () => {
-    slideUp(gridItemList4, "counter4", gridItem4);
-  });
-  gridItem6Left.addEventListener("click", () => {
-    slideDown(gridItemList6, "counter6", gridItem6);
-  });
-  gridItem6Right.addEventListener("click", () => {
-    slideUp(gridItemList6, "counter6", gridItem6);
-  });
-  gridItem10Left.addEventListener("click", () => {
-    slideDown(gridItemList10, "counter10", gridItem10);
-  });
-  gridItem10Right.addEventListener("click", () => {
-    slideUp(gridItemList10, "counter10", gridItem10);
-  });
-  gridItem14Left.addEventListener("click", () => {
-    slideDown(gridItemList14, "counter14", gridItem14);
-  });
-  gridItem14Right.addEventListener("click", () => {
-    slideUp(gridItemList14, "counter14", gridItem14);
-  });
-  gridItem21Left.addEventListener("click", () => {
-    slideDown(gridItemList21, "counter21", gridItem21);
-  });
-  gridItem21Right.addEventListener("click", () => {
-    slideUp(gridItemList21, "counter21", gridItem21);
-  });
-  gridItem23Left.addEventListener("click", () => {
-    slideDown(gridItemList23, "counter23", gridItem23);
-  });
-  gridItem23Right.addEventListener("click", () => {
-    slideUp(gridItemList23, "counter23", gridItem23);
-  });
-  gridItem24Left.addEventListener("click", () => {
-    slideDown(gridItemList24, "counter24", gridItem24);
-  });
-  gridItem24Right.addEventListener("click", () => {
-    slideUp(gridItemList24, "counter24", gridItem24);
-  });
-  gridItem26Left.addEventListener("click", () => {
-    slideDown(gridItemList26, "counter26", gridItem26);
-  });
-  gridItem26Right.addEventListener("click", () => {
-    slideUp(gridItemList26, "counter26", gridItem26);
-  });
-  gridItem30Left.addEventListener("click", () => {
-    slideDown(gridItemList30, "counter30", gridItem30);
-  });
-  gridItem30Right.addEventListener("click", () => {
-    slideUp(gridItemList30, "counter30", gridItem30);
-  });
-  gridItem31Left.addEventListener("click", () => {
-    slideDown(gridItemList31, "counter31", gridItem31);
-  });
-  gridItem31Right.addEventListener("click", () => {
-    slideUp(gridItemList31, "counter31", gridItem31);
-  });
+let listenDesktop = () => {
+  if (window.innerWidth > 480) {
+    gridItem2Left.addEventListener("click", () => {
+      slideDown(gridItemList2, "counter2", gridItem2);
+    });
+    gridItem2Right.addEventListener("click", () => {
+      slideUp(gridItemList2, "counter2", gridItem2);
+    });
+    gridItem4Left.addEventListener("click", () => {
+      slideDown(gridItemList4, "counter4", gridItem4);
+    });
+    gridItem4Right.addEventListener("click", () => {
+      slideUp(gridItemList4, "counter4", gridItem4);
+    });
+    gridItem6Left.addEventListener("click", () => {
+      slideDown(gridItemList6, "counter6", gridItem6);
+    });
+    gridItem6Right.addEventListener("click", () => {
+      slideUp(gridItemList6, "counter6", gridItem6);
+    });
+    gridItem10Left.addEventListener("click", () => {
+      slideDown(gridItemList10, "counter10", gridItem10);
+    });
+    gridItem10Right.addEventListener("click", () => {
+      slideUp(gridItemList10, "counter10", gridItem10);
+    });
+    gridItem14Left.addEventListener("click", () => {
+      slideDown(gridItemList14, "counter14", gridItem14);
+    });
+    gridItem14Right.addEventListener("click", () => {
+      slideUp(gridItemList14, "counter14", gridItem14);
+    });
+    gridItem21Left.addEventListener("click", () => {
+      slideDown(gridItemList21, "counter21", gridItem21);
+    });
+    gridItem21Right.addEventListener("click", () => {
+      slideUp(gridItemList21, "counter21", gridItem21);
+    });
+    gridItem23Left.addEventListener("click", () => {
+      slideDown(gridItemList23, "counter23", gridItem23);
+    });
+    gridItem23Right.addEventListener("click", () => {
+      slideUp(gridItemList23, "counter23", gridItem23);
+    });
+    gridItem24Left.addEventListener("click", () => {
+      slideDown(gridItemList24, "counter24", gridItem24);
+    });
+    gridItem24Right.addEventListener("click", () => {
+      slideUp(gridItemList24, "counter24", gridItem24);
+    });
+    gridItem26Left.addEventListener("click", () => {
+      slideDown(gridItemList26, "counter26", gridItem26);
+    });
+    gridItem26Right.addEventListener("click", () => {
+      slideUp(gridItemList26, "counter26", gridItem26);
+    });
+    gridItem30Left.addEventListener("click", () => {
+      slideDown(gridItemList30, "counter30", gridItem30);
+    });
+    gridItem30Right.addEventListener("click", () => {
+      slideUp(gridItemList30, "counter30", gridItem30);
+    });
+    gridItem31Left.addEventListener("click", () => {
+      slideDown(gridItemList31, "counter31", gridItem31);
+    });
+    gridItem31Right.addEventListener("click", () => {
+      slideUp(gridItemList31, "counter31", gridItem31);
+    });
+  }
 };
 
 // slide functions
@@ -211,4 +213,56 @@ let slideUp = (pictureList, counter, slider) => {
   slider.style.backgroundImage = `url("${pictureList[window[counter]]}")`;
 };
 
-listen();
+listenDesktop();
+
+let touchstartX = 0;
+let touchstartY = 0;
+let touchendX = 0;
+let touchendY = 0;
+
+function handleGesture() {
+  if (touchendX <= touchstartX) {
+    console.log("Swiped left");
+  }
+
+  if (touchendX >= touchstartX) {
+    console.log("Swiped right");
+  }
+
+  if (touchendY <= touchstartY) {
+    console.log("Swiped up");
+  }
+
+  if (touchendY >= touchstartY) {
+    console.log("Swiped down");
+  }
+
+  if (touchendY === touchstartY) {
+    console.log("Tap");
+  }
+}
+
+//  adding mobile swipe support
+let listenMobile = () => {
+  if (window.innerWidth <= 480) {
+    gridItem2.addEventListener(
+      "touchstart",
+      function(event) {
+        touchstartX = event.changedTouches[0].screenX;
+        touchstartY = event.changedTouches[0].screenY;
+      },
+      false
+    );
+
+    gridItem2.addEventListener(
+      "touchend",
+      event => {
+        touchendX = event.changedTouches[0].screenX;
+        touchendY = event.changedTouches[0].screenY;
+        handleGesture();
+        slideDown(gridItemList2, "counter2", gridItem2);
+      },
+      false
+    );
+  }
+};
