f1eb7737a646f6a1c59bf1e53f4c3fd01d6dec0a
koebe1 <koe.be.pay@web.de>
2021-08-13 11:46:31 +0200

    added swipe support for lab mobile version

--- scripts/lab.js
+++ scripts/lab.js
@@ -1,5 +1,5 @@
 // grid-item-1
-const gridItem1 = document.getElementById("1");
+const gridItem1 = document.getElementById("slider1");
 const gridItem1Left = document.getElementById("grid-item-1-left");
 const gridItem1Right = document.getElementById("grid-item-1-right");
 window["counter1"] = 0;
@@ -11,7 +11,7 @@ const gridItemList1 = [
 ];
 
 // grid-item-2
-const gridItem2 = document.getElementById("2");
+const gridItem2 = document.getElementById("slider2");
 const gridItem2Left = document.getElementById("grid-item-2-left");
 const gridItem2Right = document.getElementById("grid-item-2-right");
 window["counter2"] = 0;
@@ -21,7 +21,7 @@ const gridItemList2 = [
   "../pictures/lab/02_rose_ice.jpg"
 ];
 // grid-item-3
-const gridItem3 = document.getElementById("3");
+const gridItem3 = document.getElementById("slider3");
 const gridItem3Left = document.getElementById("grid-item-3-left");
 const gridItem3Right = document.getElementById("grid-item-3-right");
 window["counter3"] = 0;
@@ -32,7 +32,7 @@ const gridItemList3 = [
 ];
 
 // grid-item-6
-const gridItem6 = document.getElementById("6");
+const gridItem6 = document.getElementById("slider6");
 const gridItem6Left = document.getElementById("grid-item-6-left");
 const gridItem6Right = document.getElementById("grid-item-6-right");
 window["counter6"] = 0;
@@ -45,7 +45,7 @@ const gridItemList6 = [
 ];
 
 // grid-item-11
-const gridItem11 = document.getElementById("11");
+const gridItem11 = document.getElementById("slider11");
 const gridItem11Left = document.getElementById("grid-item-11-left");
 const gridItem11Right = document.getElementById("grid-item-11-right");
 window["counter11"] = 0;
@@ -57,7 +57,7 @@ const gridItemList11 = [
 ];
 
 // grid-item-13
-const gridItem13 = document.getElementById("13");
+const gridItem13 = document.getElementById("slider13");
 const gridItem13Left = document.getElementById("grid-item-13-left");
 const gridItem13Right = document.getElementById("grid-item-13-right");
 window["counter13"] = 0;
@@ -68,7 +68,7 @@ const gridItemList13 = [
 ];
 
 // grid-item-20
-const gridItem20 = document.getElementById("20");
+const gridItem20 = document.getElementById("slider20");
 const gridItem20Left = document.getElementById("grid-item-20-left");
 const gridItem20Right = document.getElementById("grid-item-20-right");
 window["counter20"] = 0;
@@ -78,7 +78,7 @@ const gridItemList20 = [
   "../pictures/lab/02_cover.jpg"
 ];
 // grid-item-21
-const gridItem21 = document.getElementById("21");
+const gridItem21 = document.getElementById("slider21");
 const gridItem21Left = document.getElementById("grid-item-21-left");
 const gridItem21Right = document.getElementById("grid-item-21-right");
 window["counter21"] = 0;
@@ -88,7 +88,7 @@ const gridItemList21 = [
   "../pictures/lab/02_hose.jpg"
 ];
 // grid-item-24
-const gridItem24 = document.getElementById("24");
+const gridItem24 = document.getElementById("slider24");
 const gridItem24Left = document.getElementById("grid-item-24-left");
 const gridItem24Right = document.getElementById("grid-item-24-right");
 window["counter24"] = 0;
@@ -96,7 +96,7 @@ window["counter24"] = 0;
 const gridItemList24 = ["../pictures/lab/01_b.jpg", "../pictures/lab/02_b.jpg"];
 
 // grid-item-25
-const gridItem25 = document.getElementById("25");
+const gridItem25 = document.getElementById("slider25");
 const gridItem25Left = document.getElementById("grid-item-25-left");
 const gridItem25Right = document.getElementById("grid-item-25-right");
 window["counter25"] = 0;
@@ -107,7 +107,7 @@ const gridItemList25 = [
 ];
 
 // grid-item-34
-const gridItem34 = document.getElementById("34");
+const gridItem34 = document.getElementById("slider34");
 const gridItem34Left = document.getElementById("grid-item-34-left");
 const gridItem34Right = document.getElementById("grid-item-34-right");
 window["counter34"] = 0;
@@ -117,73 +117,252 @@ const gridItemList34 = [
   "../pictures/lab/02_aleatorik.jpg"
 ];
 
-let listen = () => {
-  gridItem1Left.addEventListener("click", () => {
-    slideDown(gridItemList1, "counter1", gridItem1);
-  });
-  gridItem1Right.addEventListener("click", () => {
-    slideUp(gridItemList1, "counter1", gridItem1);
-  });
-  gridItem2Left.addEventListener("click", () => {
-    slideDown(gridItemList2, "counter2", gridItem2);
-  });
-  gridItem2Right.addEventListener("click", () => {
-    slideUp(gridItemList2, "counter2", gridItem2);
-  });
-  gridItem3Left.addEventListener("click", () => {
-    slideDown(gridItemList3, "counter3", gridItem3);
-  });
-  gridItem3Right.addEventListener("click", () => {
-    slideUp(gridItemList3, "counter3", gridItem3);
-  });
-  gridItem6Left.addEventListener("click", () => {
-    slideDown(gridItemList6, "counter6", gridItem6);
-  });
-  gridItem6Right.addEventListener("click", () => {
-    slideUp(gridItemList6, "counter6", gridItem6);
-  });
-  gridItem11Left.addEventListener("click", () => {
-    slideDown(gridItemList11, "counter11", gridItem11);
-  });
-  gridItem11Right.addEventListener("click", () => {
-    slideUp(gridItemList11, "counter11", gridItem11);
-  });
-  gridItem13Left.addEventListener("click", () => {
-    slideDown(gridItemList13, "counter13", gridItem13);
-  });
-  gridItem13Right.addEventListener("click", () => {
-    slideUp(gridItemList13, "counter13", gridItem13);
-  });
-  gridItem20Left.addEventListener("click", () => {
-    slideDown(gridItemList20, "counter20", gridItem20);
-  });
-  gridItem20Right.addEventListener("click", () => {
-    slideUp(gridItemList20, "counter20", gridItem20);
-  });
-  gridItem21Left.addEventListener("click", () => {
-    slideDown(gridItemList21, "counter21", gridItem21);
-  });
-  gridItem21Right.addEventListener("click", () => {
-    slideUp(gridItemList21, "counter21", gridItem21);
-  });
-  gridItem24Left.addEventListener("click", () => {
-    slideDown(gridItemList24, "counter24", gridItem24);
-  });
-  gridItem24Right.addEventListener("click", () => {
-    slideUp(gridItemList24, "counter24", gridItem24);
-  });
-  gridItem25Left.addEventListener("click", () => {
-    slideDown(gridItemList25, "counter25", gridItem25);
-  });
-  gridItem25Right.addEventListener("click", () => {
-    slideUp(gridItemList25, "counter25", gridItem25);
-  });
-  gridItem34Left.addEventListener("click", () => {
-    slideDown(gridItemList34, "counter34", gridItem34);
-  });
-  gridItem34Right.addEventListener("click", () => {
-    slideUp(gridItemList34, "counter34", gridItem34);
-  });
+let swipeWrap = document.querySelectorAll(".swipe-wrap");
+let gridItem = document.querySelectorAll(".slider");
+let gridItemClickLeft = document.querySelectorAll(".grid-item-click-left");
+let gridItemClickRight = document.querySelectorAll(".grid-item-click-right");
+
+let listenDesktop = () => {
+  if (window.innerWidth > 480) {
+    // set swipeWrapper property "display" to "none"
+    for (let element of swipeWrap) {
+      element.style.display = "none";
+    }
+    // remove class "swipe" from grid items so background image gets visible for desktop version
+    for (let element of gridItem) {
+      element.classList.remove("swipe");
+      element.style.display = "flex";
+    }
+    // add click surfaces for slider in desktop version
+    for (let element of gridItemClickLeft) {
+      element.style.display = "block";
+    }
+    for (let element of gridItemClickRight) {
+      element.style.display = "block";
+    }
+
+    gridItem1Left.addEventListener("click", () => {
+      slideDown(gridItemList1, "counter1", gridItem1);
+    });
+    gridItem1Right.addEventListener("click", () => {
+      slideUp(gridItemList1, "counter1", gridItem1);
+    });
+    gridItem2Left.addEventListener("click", () => {
+      slideDown(gridItemList2, "counter2", gridItem2);
+    });
+    gridItem2Right.addEventListener("click", () => {
+      slideUp(gridItemList2, "counter2", gridItem2);
+    });
+    gridItem3Left.addEventListener("click", () => {
+      slideDown(gridItemList3, "counter3", gridItem3);
+    });
+    gridItem3Right.addEventListener("click", () => {
+      slideUp(gridItemList3, "counter3", gridItem3);
+    });
+    gridItem6Left.addEventListener("click", () => {
+      slideDown(gridItemList6, "counter6", gridItem6);
+    });
+    gridItem6Right.addEventListener("click", () => {
+      slideUp(gridItemList6, "counter6", gridItem6);
+    });
+    gridItem11Left.addEventListener("click", () => {
+      slideDown(gridItemList11, "counter11", gridItem11);
+    });
+    gridItem11Right.addEventListener("click", () => {
+      slideUp(gridItemList11, "counter11", gridItem11);
+    });
+    gridItem13Left.addEventListener("click", () => {
+      slideDown(gridItemList13, "counter13", gridItem13);
+    });
+    gridItem13Right.addEventListener("click", () => {
+      slideUp(gridItemList13, "counter13", gridItem13);
+    });
+    gridItem20Left.addEventListener("click", () => {
+      slideDown(gridItemList20, "counter20", gridItem20);
+    });
+    gridItem20Right.addEventListener("click", () => {
+      slideUp(gridItemList20, "counter20", gridItem20);
+    });
+    gridItem21Left.addEventListener("click", () => {
+      slideDown(gridItemList21, "counter21", gridItem21);
+    });
+    gridItem21Right.addEventListener("click", () => {
+      slideUp(gridItemList21, "counter21", gridItem21);
+    });
+    gridItem24Left.addEventListener("click", () => {
+      slideDown(gridItemList24, "counter24", gridItem24);
+    });
+    gridItem24Right.addEventListener("click", () => {
+      slideUp(gridItemList24, "counter24", gridItem24);
+    });
+    gridItem25Left.addEventListener("click", () => {
+      slideDown(gridItemList25, "counter25", gridItem25);
+    });
+    gridItem25Right.addEventListener("click", () => {
+      slideUp(gridItemList25, "counter25", gridItem25);
+    });
+    gridItem34Left.addEventListener("click", () => {
+      slideDown(gridItemList34, "counter34", gridItem34);
+    });
+    gridItem34Right.addEventListener("click", () => {
+      slideUp(gridItemList34, "counter34", gridItem34);
+    });
+  }
+};
+
+let listenMobile = () => {
+  if (window.innerWidth <= 480) {
+    // remove elements and add elements and classes for mobile slider support
+    for (let element of swipeWrap) {
+      element.style.display = "block";
+    }
+
+    for (let element of gridItem) {
+      element.classList.add("swipe");
+      element.style.display = "block";
+    }
+
+    for (let element of gridItemClickLeft) {
+      element.style.display = "none";
+    }
+    for (let element of gridItemClickRight) {
+      element.style.display = "none";
+    }
+
+    // slider 1
+    window.mySwipe = new Swipe(document.getElementById("slider1"), {
+      startSlide: 0,
+      speed: 400,
+      draggable: false,
+      continuous: true,
+      disableScroll: false,
+      stopPropagation: false,
+      ignore: ".scroller",
+      callback: function(index, elem, dir) {},
+      transitionEnd: function(index, elem) {}
+    });
+
+    // slider 2
+    window.mySwipe = new Swipe(document.getElementById("slider2"), {
+      startSlide: 0,
+      speed: 400,
+      draggable: false,
+      continuous: true,
+      disableScroll: false,
+      stopPropagation: false,
+      ignore: ".scroller",
+      callback: function(index, elem, dir) {},
+      transitionEnd: function(index, elem) {}
+    });
+    // slider 3
+    window.mySwipe = new Swipe(document.getElementById("slider3"), {
+      startSlide: 0,
+      speed: 400,
+      draggable: false,
+      continuous: true,
+      disableScroll: false,
+      stopPropagation: false,
+      ignore: ".scroller",
+      callback: function(index, elem, dir) {},
+      transitionEnd: function(index, elem) {}
+    });
+    // slider 6
+    window.mySwipe = new Swipe(document.getElementById("slider6"), {
+      startSlide: 0,
+      speed: 400,
+      draggable: false,
+      continuous: true,
+      disableScroll: false,
+      stopPropagation: false,
+      ignore: ".scroller",
+      callback: function(index, elem, dir) {},
+      transitionEnd: function(index, elem) {}
+    });
+    // slider 11
+    window.mySwipe = new Swipe(document.getElementById("slider11"), {
+      startSlide: 0,
+      speed: 400,
+      draggable: false,
+      continuous: true,
+      disableScroll: false,
+      stopPropagation: false,
+      ignore: ".scroller",
+      callback: function(index, elem, dir) {},
+      transitionEnd: function(index, elem) {}
+    });
+    // slider 13
+    window.mySwipe = new Swipe(document.getElementById("slider13"), {
+      startSlide: 0,
+      speed: 400,
+      draggable: false,
+      continuous: true,
+      disableScroll: false,
+      stopPropagation: false,
+      ignore: ".scroller",
+      callback: function(index, elem, dir) {},
+      transitionEnd: function(index, elem) {}
+    });
+    // slider 20
+    window.mySwipe = new Swipe(document.getElementById("slider20"), {
+      startSlide: 0,
+      speed: 400,
+      draggable: false,
+      continuous: true,
+      disableScroll: false,
+      stopPropagation: false,
+      ignore: ".scroller",
+      callback: function(index, elem, dir) {},
+      transitionEnd: function(index, elem) {}
+    });
+    // slider 21
+    window.mySwipe = new Swipe(document.getElementById("slider21"), {
+      startSlide: 0,
+      speed: 400,
+      draggable: false,
+      continuous: true,
+      disableScroll: false,
+      stopPropagation: false,
+      ignore: ".scroller",
+      callback: function(index, elem, dir) {},
+      transitionEnd: function(index, elem) {}
+    });
+    // slider 24
+    window.mySwipe = new Swipe(document.getElementById("slider24"), {
+      startSlide: 0,
+      speed: 400,
+      draggable: false,
+      continuous: true,
+      disableScroll: false,
+      stopPropagation: false,
+      ignore: ".scroller",
+      callback: function(index, elem, dir) {},
+      transitionEnd: function(index, elem) {}
+    });
+    // slider 25
+    window.mySwipe = new Swipe(document.getElementById("slider25"), {
+      startSlide: 0,
+      speed: 400,
+      draggable: false,
+      continuous: true,
+      disableScroll: false,
+      stopPropagation: false,
+      ignore: ".scroller",
+      callback: function(index, elem, dir) {},
+      transitionEnd: function(index, elem) {}
+    });
+    // slider 34
+    window.mySwipe = new Swipe(document.getElementById("slider34"), {
+      startSlide: 0,
+      speed: 400,
+      draggable: false,
+      continuous: true,
+      disableScroll: false,
+      stopPropagation: false,
+      ignore: ".scroller",
+      callback: function(index, elem, dir) {},
+      transitionEnd: function(index, elem) {}
+    });
+  }
 };
 
 // slide functions
@@ -205,4 +384,7 @@ let slideUp = (pictureList, counter, slider) => {
   slider.style.backgroundImage = `url("${pictureList[window[counter]]}")`;
 };
 
-listen();
+window.addEventListener("resize", () => listenMobile());
+window.addEventListener("resize", () => listenDesktop());
+listenDesktop();
+listenMobile();
