f4caa29e6dabfc33dc34dc41aaf4638beff46600
koebe1 <koe.be.pay@web.de>
2021-08-14 21:59:13 +0200

    fixed slider-sub on projects

--- scripts/projects.js
+++ scripts/projects.js
@@ -12,48 +12,49 @@ const swiper = new Swiper(".swiper-container", {
   }
 });
 
-// function for custom cursor
+if (window.innerWidth <= 480) {
+  // function for custom cursor
+  let swiperContainer = document.querySelector(".swiper-container");
+  swiperContainer.addEventListener("mousemove", function(e) {
+    let width = swiperContainer.clientWidth;
+    let x = e.pageX - this.offsetLeft;
 
-let swiperContainer = document.querySelector(".swiper-container");
-swiperContainer.addEventListener("mousemove", function(e) {
-  let width = swiperContainer.clientWidth;
-  let x = e.pageX - this.offsetLeft;
+    swiperContainer.classList.toggle(
+      "arrow-left",
+      //  calculate on which side of the window the element is
+      x / width < 0.5,
+      x / width > 0.5
+    );
 
-  swiperContainer.classList.toggle(
-    "arrow-left",
-    //  calculate on which side of the window the element is
-    x / width < 0.5,
-    x / width > 0.5
-  );
+    swiperContainer.classList.toggle(
+      "arrow-right",
+      x / width >= 0.5,
+      x / width < 0.5
+    );
+  });
 
-  swiperContainer.classList.toggle(
-    "arrow-right",
-    x / width >= 0.5,
-    x / width < 0.5
-  );
-});
+  // function for slide change on click
+  swiperContainer.addEventListener("click", () => {
+    // click left side
+    if (swiperContainer.classList.contains("arrow-left")) {
+      swiper.slidePrev();
+    } else if (swiperContainer.classList.contains("arrow-right")) {
+      swiper.slideNext();
+    }
+  });
 
-// function for slide change on click
-swiperContainer.addEventListener("click", () => {
-  // click left side
-  if (swiperContainer.classList.contains("arrow-left")) {
-    swiper.slidePrev();
-  } else if (swiperContainer.classList.contains("arrow-right")) {
-    swiper.slideNext();
-  }
-});
-
-// info-card
-let infoContainer = document.querySelector(".info-container");
-let info = document.getElementById("slider-sub-info");
-info.addEventListener("click", () => {
-  if (infoContainer.classList.contains("info-container-visible")) {
-    infoContainer.classList.add("info-container-hidden");
-    infoContainer.classList.remove("info-container-visible");
-    info.innerHTML = "INFO";
-  } else {
-    infoContainer.classList.add("info-container-visible");
-    infoContainer.classList.remove("info-container-hidden");
-    info.innerHTML = "CLOSE";
-  }
-});
+  // info-card
+  let infoContainer = document.querySelector(".info-container");
+  let info = document.getElementById("slider-sub-info");
+  info.addEventListener("click", () => {
+    if (infoContainer.classList.contains("info-container-visible")) {
+      infoContainer.classList.add("info-container-hidden");
+      infoContainer.classList.remove("info-container-visible");
+      info.innerHTML = "INFO";
+    } else {
+      infoContainer.classList.add("info-container-visible");
+      infoContainer.classList.remove("info-container-hidden");
+      info.innerHTML = "CLOSE";
+    }
+  });
+}
