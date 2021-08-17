6ba0395e3fef97db14bbf8b922781a3ab130bee4
koebe1 <koe.be.pay@web.de>
2021-08-17 17:37:43 +0200

    added a mobile navbar to index for UI testing

--- scripts/nav.js
+++ scripts/nav.js
@@ -11,10 +11,6 @@ let closeContainer = document.querySelector(".close-container");
 let vh = window.innerHeight / 100;
 
 if (window.innerWidth <= 480) {
-  navMobile.style.height = `${vh * 100}px`;
-  navMobile.style.width = `100vw`;
-  closeContainer.style.height = `${vh * 9.5}px`;
-
   dotsMobile.addEventListener("click", () => {
     body.style.backgroundColor = "#000";
     body.style.zIndex = "999";
@@ -24,6 +20,9 @@ if (window.innerWidth <= 480) {
     main.style.display = "none";
     footer.style.display = "none";
     nav.style.padding = "0";
+    navMobile.style.height = `${vh * 100}px`;
+    navMobile.style.width = `100vw`;
+    closeContainer.style.height = `${vh * 9.5}px`;
   });
   close.addEventListener("click", () => {
     navMobile.style.display = "none";
